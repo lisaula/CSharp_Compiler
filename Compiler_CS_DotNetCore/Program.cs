9d19f272195572924c9f715b84081be05e5124e8
Luis Carlos Isaula <luisca_elfather@hotmail.com>
2017-05-22 14:37:16 -0600

    prueba raim

--- Compiler_CS_DotNetCore/Program.cs
+++ Compiler_CS_DotNetCore/Program.cs
@@ -11,9 +11,12 @@ namespace Compiler
             var s = @"
 public class kevin : Nexer{
     public void metodo(){
-        for(i.system.hola nombre =0; i < 10; i++, x++){
-            System.Console.Writeline(""hola"");
-        }
+        int[][] jaggedArray2 = new int[][] 
+        {
+        new int[] {1,3,5,7,9},
+        new int[] {0,2,4,6},
+        new int[] {11,22}
+        };
     }
 }
 ";
