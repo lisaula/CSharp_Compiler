71d95531c183696c18250f76b42a0b6e35680c1a
Luis Carlos Isaula <luisca_elfather@hotmail.com>
2017-06-18 21:00:41 -0600

    finished

--- Compiler_CS_DotNetCore/Javascript/prueba.js
+++ Compiler_CS_DotNetCore/Javascript/prueba.js
@@ -1,4 +1,3 @@
-let GlobalNamespace = {};
 GlobalNamespace.System = {};
 GlobalNamespace.System.IO = {};
 GlobalNamespace.Object = class {
@@ -37,6 +36,14 @@ GlobalNamespace.CharType = class {
     static ParseStringType(s) {
         return s;
     }
+}
+GlobalNamespace.DictionaryTypeNode = class {
+    constructor() {
+        let argumentos = Array.from(arguments);
+        let argus = argumentos.slice(1);
+        if (argumentos.length > 1) this[arguments[0]](...argus);
+    }
+    ToString() {}
 }
 GlobalNamespace.FloatType = class {
     constructor() {
@@ -79,9 +86,12 @@ GlobalNamespace.System.Console = class {
     }
     static WriteLineStringType(message) {
         console.log(message);
+     }
+    static ReadLine() {
+
     }
-    static ReadLine() {}
 }
+GlobalNamespace.System.Console.Out = new GlobalNamespace.System.IO.TextWriter ;
 GlobalNamespace.System.IO.TextWriter = class {
     constructor() {
         let argumentos = Array.from(arguments);
@@ -92,7 +102,6 @@ GlobalNamespace.System.IO.TextWriter = class {
         console.log(message);
     }
 }
-GlobalNamespace.System.Console.Out = new GlobalNamespace.System.IO.TextWriter();
 GlobalNamespace.System.IO.TextReader = class {
     constructor() {
         let argumentos = Array.from(arguments);
@@ -101,42 +110,3 @@ GlobalNamespace.System.IO.TextReader = class {
     }
     static ReadLine() {}
 }
-GlobalNamespace.sort = class {
-    sort() {}
-    constructor() {
-        this.prueba = 5;
-        let argumentos = Array.from(arguments);
-        let argus = argumentos.slice(1);
-        if (argumentos.length > 1) this[arguments[0]](...argus);
-    }
-    static IntArrayMinIntTypeArray1DIntTypeIntType(data, start, size) {
-        let minPos = start;
-        for (let pos = (start) + (1);
-            (pos) < (size); pos++) {
-            if ((data[pos]) < (data[minPos])) {
-                minPos = pos
-            }
-        };
-        return minPos;
-    }
-    static IntArraySelectionSortIntTypeArray1DIntType(data, size) {
-        let i = null;
-        let N = size;
-        GlobalNamespace.System.Console.WriteLineStringType("Hola");
-        let n = GlobalNamespace.IntType.ParseStringType("5");
-        for (i = 0;
-            (i) < ((N) - (1)); i++) {
-            let k = this.IntArrayMinIntTypeArray1DIntTypeIntType(data, i, this.prueba);
-            if ((i) != (k)) {
-                this.exchangeIntTypeArray1DIntTypeIntType(data, i, k)
-            };
-        };
-    }
-    static exchangeIntTypeArray1DIntTypeIntType(data, m, n) {
-        let temporary = null;
-        temporary = data[m];
-        data[m] = data[n];
-        data[n] = temporary;
-    }
-}
-module.exports = GlobalNamespace;
